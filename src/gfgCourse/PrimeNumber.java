ea8b586f5901944107e2c7d62c302ce4b5768e1e
Saket105 <saketcool35@gmail.com>
2024-07-19 00:10:25 +0530

    Prime Number problem

--- src/gfgCourse/PrimeNumber.java
+++ src/gfgCourse/PrimeNumber.java
@@ -10,9 +10,11 @@ package gfgCourse;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        System.out.println(isPrime(17));
+        System.out.println(isPrime(121));
+        System.out.println(isPrimeOptimized(1031));
     }
 
+    //TODO: Brute Force
     public static boolean isPrime(int n){
        if (n==1)
            return false;
@@ -21,5 +23,20 @@ public class PrimeNumber {
                return false;
        }
        return true;
+    }
+
+    //TODO: Optimized Solution
+    public static boolean isPrimeOptimized(int n){
+        if (n==1)
+            return false;
+        if (n==2 || n==3)
+            return true;
+        if (n%2==0 || n%3==0)
+            return false;
+        for (int i=5;i*i<=n;i=i+6){
+            if (n%i==0 || n%(i+2)==0)
+                return false;
+        }
+        return true;
     }
 }
